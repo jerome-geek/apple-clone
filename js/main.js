2a54a5cb4d08eedcb680285c22fe4e44151347c9
jerome <jerome@geekstudio.co.kr>
2023-01-30 01:02:07 +0900

    :sparkles: 현재 활성 씬 반영

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 (() => {
+    let yOffset = 0; // window.pageYOffset대신 쓸 변수
+    let prevScrollHeight = 0; // 현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이의 합
+    let currentScene = 0; // 현재 활성화된(눈 앞에 보고있는) 씬(scroll-section)
+
     const sceneInfo = [
         {
             // 0
@@ -47,9 +51,45 @@
                 i
             ].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
         }
+
+        yOffset = window.pageYOffset;
+
+        let totalScrollHeight = 0;
+        for (let i = 0; i < sceneInfo.length; i++) {
+            totalScrollHeight += sceneInfo[i].scrollHeight;
+            if (totalScrollHeight >= yOffset) {
+                currentScene = i;
+                break;
+            }
+        }
+        document.body.setAttribute('id', `show-scene-${currentScene}`);
     }
 
-    window.addEventListener('resize', setLayout);
+    function scrollLoop() {
+        prevScrollHeight = 0;
+        for (let i = 0; i < currentScene; i++) {
+            prevScrollHeight += sceneInfo[i].scrollHeight;
+        }
 
-    setLayout();
+        if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+            currentScene++;
+            document.body.setAttribute('id', `show-scene-${currentScene}`);
+        }
+
+        if (yOffset < prevScrollHeight) {
+            // 브라우저 바운스 효과로 인해 마이너스가 되는 것을 방지 (모바일)
+            if (currentScene === 0) {
+                return;
+            }
+            currentScene--;
+        }
+    }
+
+    window.addEventListener('scroll', () => {
+        yOffset = window.pageYOffset;
+        scrollLoop();
+    });
+    window.addEventListener('DOMContentLoaded', setLayout);
+    window.addEventListener('load', setLayout);
+    window.addEventListener('resize', setLayout);
 })();
